db2a84d29d41c98595e1f808db15fa6e287e9351
Pranav Bodawala <b.pranav.k@gmail.com>
2020-06-02 21:53:20 -0600

    add circles array and draw a bunch of circles

--- canvas.js
+++ canvas.js
@@ -14,15 +14,18 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 let MAX_SPEED = 10;
+let MIN_RADIUS = 2;
+let MAX_RADIUS = 40;
+let NUMBER_OF_CIRCLES = 300;
 
 class Circle {
     constructor() {
-        this.radius = 30;
+        this.radius = MIN_RADIUS;
         this.x = random(0 + this.radius, innerWidth - this.radius);
         this.y = random(0 + this.radius, innerHeight - this.radius);
         this.dx = random(1, MAX_SPEED);
         this.dy = random(1, MAX_SPEED);
-        this.color = "red";
+        this.color = "black";
     }
 
     draw() {
@@ -49,15 +52,17 @@ class Circle {
     }
 }
 
-let circle = new Circle();
-// circle.draw();
+let circles = [];
+for (let i = 0; i < NUMBER_OF_CIRCLES; ++i) {
+    circles.push(new Circle());
+}
 
 function animate() {
     requestAnimationFrame(animate);
 
     ctx?.clearRect(0, 0, innerWidth, innerHeight);
 
-    circle.update();
+    circles.forEach((circle) => circle.update());
 }
 
 animate();
