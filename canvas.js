1eb19f02e14a7beed4eb2bd0f747db4ea9a90e7e
Pranav Bodawala <b.pranav.k@gmail.com>
2020-06-02 19:07:15 -0600

    initialize canvas

--- canvas.js
+++ canvas.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+let canvas = document.querySelector('canvas');
+if (!canvas) {
+    throw new Error("Canvas could not be initialized");
+}
+
+let ctx = canvas.getContext('2d');
+if (!ctx) {
+    throw new Error("Context could not be initialized");
+}
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
